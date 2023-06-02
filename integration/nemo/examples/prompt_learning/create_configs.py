f6c657b15b6df3d9e48a66655f28df4a177a6422
Holger Roth <hroth@nvidia.com>
2023-06-02 01:32:19 +0000

    separate model and data root

--- integration/nemo/examples/prompt_learning/create_configs.py
+++ integration/nemo/examples/prompt_learning/create_configs.py
@@ -43,8 +43,11 @@ def main():
     parser.add_argument("--num_rounds", type=int, help="Number of FL rounds.", default=1)
     parser.add_argument("--devices", type=int, help="Number of GPU devices per client.", default=1)
     parser.add_argument(
-        "--root_dir", type=str, help="Root folder containing the example with data and models.", default=os.getcwd()
+        "--model_root_dir", type=str, help="Root folder containing the models.", default=os.getcwd()
     )
+    parser.add_argument(
+        "--data_root_dir", type=str, help="Root folder containing the data.", default=os.getcwd()
+    )    
     parser.add_argument(
         "--val_ds_files",
         type=str,
@@ -82,9 +85,9 @@ def main():
 
         # modify client configs
         client_cfg = load_config(client_cfg_file)
-        client_cfg["ROOT_DIR"] = args.root_dir
-        client_cfg["train_ds_files"] = os.path.join(args.root_dir, f"{args.train_ds_files_prefix}{i}{args.train_ds_files_suffix}")
-        client_cfg["val_ds_files"] = os.path.join(args.root_dir, args.val_ds_files)
+        client_cfg["ROOT_DIR"] = args.model_root_dir
+        client_cfg["train_ds_files"] = os.path.join(args.data_root_dir, f"{args.train_ds_files_prefix}{i}{args.train_ds_files_suffix}")
+        client_cfg["val_ds_files"] = os.path.join(args.data_root_dir, args.val_ds_files)
         client_cfg["aggregation_epochs"] = args.aggregation_epochs
         if args.devices > 1:
             client_cfg["devices"] = args.devices
