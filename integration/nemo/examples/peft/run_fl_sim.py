9b5afc6c82bde1d08bf85caf1bb710152d0716f3
Holger Roth <hroth@nvidia.com>
2023-08-18 00:05:46 +0000

    disable restore from ckpt

--- integration/nemo/examples/peft/run_fl_sim.py
+++ integration/nemo/examples/peft/run_fl_sim.py
@@ -1,5 +1,24 @@
+import os
+import glob
+import subprocess
 from nvflare import SimulatorRunner
 
+data_root = "/tmp/data"
+
+def clean_files(data_root, ext):
+    files = glob.glob(os.path.join(data_root, "*", ext), recursive=True)
+    for file in files:
+        print(file)
+        os.remove(file)
+
+def clean_memmap(data_root):
+    clean_files(data_root, "*.npy")
+    clean_files(data_root, "*.info")
+
+# Clean temporary data
+clean_memmap(data_root)
+
+# Start FL simulation
 simulator = SimulatorRunner(
     job_folder="jobs/peft",
     workspace="/tmp/nvflare/nemo/peft_fedavg_345M",
