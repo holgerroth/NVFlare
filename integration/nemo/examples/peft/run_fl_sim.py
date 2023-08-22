35a8858597006c925efffff64a4c66737e933ed0
Holger Roth <hroth@nvidia.com>
2023-08-22 17:13:21 +0000

    add intime model selector

--- integration/nemo/examples/peft/run_fl_sim.py
+++ integration/nemo/examples/peft/run_fl_sim.py
@@ -5,6 +5,10 @@ import glob
 import subprocess
 from nvflare import SimulatorRunner
 
+n_clients=3
+peft_scheme="lora"
+job_name=f"peft_{peft_scheme}_fedavg_345M"
+
 data_root = "/tmp/data"
 
 def clean_files(data_root, ext):
@@ -20,12 +24,21 @@ def clean_memmap(data_root):
 # Clean temporary data
 clean_memmap(data_root)
 
+# Create configurations
+subprocess.run(["python3", "create_configs.py", "--job_folder", f"jobs/{job_name}", 
+                "--num_clients", str(n_clients), 
+                "--max_steps", "200", 
+                "--val_check_interval", "100",
+                "--num_rounds", "50",
+                "--peft_scheme", peft_scheme])
+
 # Start FL simulation
 simulator = SimulatorRunner(
-    job_folder="jobs/peft",
-    workspace="/tmp/nvflare/nemo/peft_fedavg_345M",
-    n_clients=1,
-    threads=1
+    job_folder=f"jobs/{job_name}",
+    workspace=f"/tmp/nvflare/nemo/{job_name}",
+    n_clients=n_clients,
+    threads=n_clients,
+    gpu="0,1,2"
 )
 run_status = simulator.run()
 print("Simulator finished with run_status", run_status)
