6d6f0732c55d73ec7360ae863b62b56db3b3d40f
Holger Roth <hroth@nvidia.com>
2023-08-31 17:01:06 +0000

    first experiments

---
+++ integration/nemo/examples/peft_ner/run_fl_sim_tp2.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+import os
+import glob
+import sys
+import subprocess
+from nvflare import SimulatorRunner
+
+n_clients=3
+peft_scheme="lora"
+max_steps=20
+num_rounds=100
+lr=1e-4
+nemo_ckpt="megatron_gpt_345m_tp2.nemo"
+devices=2
+
+job_name=f"peft_{peft_scheme}_fedavg_345M_lr{lr}_steps{max_steps}_val10_rounds{num_rounds}_{n_clients}clients_devices{devices}"
+
+data_root = "/tmp/data"
+
+def clean_files(data_root, ext):
+    files = glob.glob(os.path.join(data_root, "*", ext), recursive=True)
+    for file in files:
+        print(file)
+        os.remove(file)
+
+def clean_memmap(data_root):
+    clean_files(data_root, "*.npy")
+    clean_files(data_root, "*.info")
+
+# Clean temporary data
+clean_memmap(data_root)
+
+# Create configurations
+try:
+    subprocess.run(["python3", "create_configs.py", "--job_folder", f"jobs/{job_name}", 
+                    "--num_clients", str(n_clients), 
+                    "--max_steps", str(max_steps), 
+                    "--val_check_interval", str(max_steps),
+                    "--num_rounds", str(num_rounds),
+                    "--lr", str(lr),
+                    "--peft_scheme", peft_scheme,
+                    "--nemo_ckpt", nemo_ckpt,
+                    "--devices", str(devices)])
+except subprocess.CalledProcessError as e:
+    raise RuntimeError(f"Create_configs failed with {e.output}")
+
+# Start FL simulation
+simulator = SimulatorRunner(
+    job_folder=f"jobs/{job_name}",
+    workspace=f"/tmp/nvflare/nemo/{job_name}",
+    n_clients=n_clients,
+    threads=n_clients,
+    gpu="[0,1],[1,2],[2,3]"
+)
+run_status = simulator.run()
+print("Simulator finished with run_status", run_status)
