935668fc98decd7e0a6147a69a3ec36b5df8c0d3
Holger Roth <hroth@nvidia.com>
2023-12-13 11:57:30 -0500

    run alpha1.0

--- examples/advanced/bionemo/task_fitting/run_sim_tap.py
+++ examples/advanced/bionemo/task_fitting/run_sim_tap.py
@@ -5,13 +5,12 @@ from nvflare import SimulatorRunner
 n_clients = 5
 
 simulator = SimulatorRunner(
-    #job_folder="jobs/local_tap_esm1nv",
-    #workspace=f"./results/local_finetune_esm1nv_enclr1e-7_maxepochs200_JoinedChains_5",
-    job_folder="jobs/fedavg_tap_esm1nv",
-    workspace=f"./results/fedavg_finetune_esm1nv_enclr1e-7_maxepochs200_JoinedChains_5",
+    job_folder="jobs/local_tap_esm1nv",
+    workspace=f"./results/local_finetune_esm1nv_enclr1e-7_maxepochs200_JoinedChains_alpha1.0",
+    #job_folder="jobs/fedavg_tap_esm1nv",
+    #workspace=f"./results/fedavg_finetune_esm1nv_enclr1e-7_maxepochs200_JoinedChains_alpha1.0",
     n_clients=n_clients,
     threads=n_clients
 )
 run_status = simulator.run()
 print("Simulator finished with run_status", run_status)
-
