9e340dac9d9e6ac7575b3027ea00d9be92986962
Holger Roth <hroth@nvidia.com>
2023-12-13 09:02:07 -0500

    compare ESM1

--- examples/advanced/bionemo/task_fitting/run_sim_tap.py
+++ examples/advanced/bionemo/task_fitting/run_sim_tap.py
@@ -5,10 +5,10 @@ from nvflare import SimulatorRunner
 n_clients = 5
 
 simulator = SimulatorRunner(
-    job_folder="jobs/local_tap_esm1nv",
-    workspace=f"./results/local_finetune_esm1nv_enclr1e-6_maxepochs500_JoinedChains",
-    #job_folder="jobs/fedavg_tap_esm1nv",
-    #workspace=f"./results/fedavg_finetune_esm1nv_enclr1e-6_maxepochs10_JoinedChains",
+    #job_folder="jobs/local_tap_esm1nv",
+    #workspace=f"./results/local_finetune_esm1nv_enclr1e-7_maxepochs200_JoinedChains",
+    job_folder="jobs/fedavg_tap_esm1nv",
+    workspace=f"./results/fedavg_finetune_esm1nv_enclr1e-7_maxepochs200_JoinedChains_3",
     n_clients=n_clients,
     threads=n_clients
 )
