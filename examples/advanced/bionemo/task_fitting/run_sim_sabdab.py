5e5a0d0cbac7133eb02a6d4b86d9b06bbe4a3d7b
Holger Roth <hroth@nvidia.com>
2023-12-19 12:48:39 -0500

    sabdab alpha 100

--- examples/advanced/bionemo/task_fitting/run_sim_sabdab.py
+++ examples/advanced/bionemo/task_fitting/run_sim_sabdab.py
@@ -6,10 +6,10 @@ n_clients = 6
 simulator = SimulatorRunner(
     #job_folder="jobs/central_sabdab_esm1nv",
     #workspace=f"./results/central_finetune_esm1nv_enclr1e-5_maxepochs500_JoinedChains_bs32",
-    #job_folder="jobs/local_sabdab_esm1nv",
-    #workspace=f"./results/local_finetune_esm1nv_enclr1e-5_maxepochs500_JoinedChains_alpha100_bs32",
-    job_folder="jobs/fedavg_sabdab_esm1nv",
-    workspace=f"./results/fedavg_finetune_esm1nv_enclr1e-5_maxepochs500_JoinedChains_alpha100_bs32",
+    job_folder="jobs/local_sabdab_esm1nv",
+    workspace=f"./results/local_finetune_esm1nv_enclr1e-5_maxepochs500_JoinedChains_alpha100_bs32",
+    #job_folder="jobs/fedavg_sabdab_esm1nv",
+    #workspace=f"./results/fedavg_finetune_esm1nv_enclr1e-5_maxepochs500_JoinedChains_alpha100_bs32",
     n_clients=n_clients,
     threads=n_clients
 )
