5d5eb2cadb19a52f6133b8f7d84023b267a1672c
Holger Roth <hroth@nvidia.com>
2025-07-10 20:16:47 -0400

    custom intime aggregation

--- examples/advanced/job_recipe/pt/job_recipe_cifar10_fedavg_intime.py
+++ examples/advanced/job_recipe/pt/job_recipe_cifar10_fedavg_intime.py
@@ -1,10 +1,37 @@
-from typing import List
 from src.cifar10_fl import Net
 
-from nvflare.apis.dxo import DataKind
-from nvflare.app_common.aggregators.intime_accumulate_model_aggregator import InTimeAccumulateWeightedAggregator
+from nvflare.app_common.abstract.fl_model import FLModel
 from nvflare.app_common.recipes.fedavg_intime import InTimeFedAvgRecipe
 from nvflare.environments.sim_environment import SimEnv
+from nvflare.app_common.aggregators.model_aggregator import ModelAggregator
+
+class MyAggregator(ModelAggregator):
+    def __init__(self):
+        super().__init__()
+        self.sum = {}    
+        self.count = 0
+    
+    def accept_model(self, model: FLModel):
+        # accept submitted model and add to the sum
+        print(f"##### MyAggregator: Accepting model with {len(model.params)} variables #####")
+        for key, value in model.params.items():
+            if key not in self.sum:
+                self.sum[key] = 0
+            self.sum[key] += value
+        self.count += 1
+
+    def aggregate_model(self) -> FLModel:
+        print(f"##### MyAggregator: Aggregating {self.count} models #####")
+
+        # compute the average
+        for key, value in self.sum.items():
+            self.sum[key] = value / self.count
+
+        # reset the sum and count
+        self.sum = {}    
+        self.count = 0
+        
+        return FLModel(params=self.sum)
 
 
 
@@ -14,7 +41,6 @@ if __name__ == "__main__":
     num_rounds = 10
     train_script = "src/cifar10_fl.py"
 
-    my_aggregator = InTimeAccumulateWeightedAggregator(expected_data_kind=DataKind.WEIGHTS)
 
     # Now, let's create an FL recipe, defining the training logic, number rounds, min_clients, for next round, etc.
     # We can also define our own aggregation function here
@@ -24,9 +50,9 @@ if __name__ == "__main__":
         train_script=train_script,
         train_args="--epochs 1 --batch_size 32",
         initial_model=Net(),
-        aggregator=my_aggregator,
+        aggregator=MyAggregator(),
     )
 
     # Use a the SimEnv to run the experiment locally.
-    recipe.run(env=SimEnv(gpu="0", workdir="/tmp/nvflare/cifar10", name="cifar10_fedavg"))
+    recipe.run(env=SimEnv(gpu="0", workdir="/tmp/nvflare/cifar10", name="cifar10_fedavg_intime"))
     # recipe.run(env=FlareEnv())
--- nvflare/environments/sim_environment.py
+++ nvflare/environments/sim_environment.py
@@ -2,6 +2,7 @@ import os
 from typing import Optional
 
 from nvflare.environments.environment import Env
+from nvflare.job_config.api import FedJob
 
 
 class SimEnv(Env):
@@ -12,10 +13,10 @@ class SimEnv(Env):
         self.workdir = workdir
         self.name = name
 
-    def run(self, job, n_clients: int = None):
+    def run(self, job: FedJob, n_clients: int = None):
         """Run the simulation."""
         job.name = self.name
         workdir = os.path.join(self.workdir, self.name)
         # Implementation would go here
         print(f"Running simulation with GPU: {self.gpu}, output dir: {workdir}")
-        job.simulator_run(workdir, gpu=self.gpu, n_clients=n_clients)
+        job.simulator_run(workdir, gpu=self.gpu, n_clients=n_clients) #, log_config="full")
