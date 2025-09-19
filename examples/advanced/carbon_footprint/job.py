4ddd8d78fca6a71f5ca84345885276f081a45194
Holger Roth <hroth@nvidia.com>
2025-09-19 19:27:32 +0000

    restore output path

--- examples/advanced/carbon_footprint/job.py
+++ examples/advanced/carbon_footprint/job.py
@@ -29,5 +29,5 @@ if __name__ == "__main__":
         job.to(runner, f"site-{i}")
 
     job.export_job("./job_configs")
-    job.simulator_run("carbon_footprint", gpu="0,1,2,3,4,5,6,7")  # runs each client on a different GPU
+    job.simulator_run("/tmp/nvflare/carbon_footprint", gpu="0,1,2,3,4,5,6,7")  # runs each client on a different GPU
     
